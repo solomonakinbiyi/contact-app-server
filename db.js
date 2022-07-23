21d47c9f99188903217da677554f07029d6538b0
solomonakinbiyi <akinbiyisolo22@gmail.com>
2022-07-23 18:11:59 +0100

    changed db url for deployment

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const pool = new Pool({
   user: process.env.USER,
   password: process.env.PASSWORD,
   port: process.env.DBPORT,
-  database: process.env.DATABASE,
+  database: process.env.DATABASE_URL,
 });
 
 module.exports = pool;
