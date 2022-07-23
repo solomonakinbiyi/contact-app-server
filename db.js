82dd7a127d8036d0d3fbbb4150ee4219917feb9e
solomonakinbiyi <akinbiyisolo22@gmail.com>
2022-07-23 23:46:56 +0100

    still troubleshooting

--- db.js
+++ db.js
@@ -10,17 +10,9 @@ const devConfig = {
 
 const proConfig = {
   connectionString: process.env.DATABASE_URL,
-  // connectionString: process.env.PG_DATABASE,
-  // ssl: true,
-  // dialect: "postgres",
-  // dialectOptions: {
-  //   ssl: { require: true },
-  // },
-  // user: process.env.PG_USER,
-  // password: process.env.PG_PASSWORD,
-  // host: process.env.PG_HOST,
-  // database: process.env.PG_DATABASE,
-  // port: process.env.PG_PORT,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 };
 
 const pool = new Pool(
