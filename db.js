dadf46e7bd9919fa205e9eaf8e4f920804ca29c6
solomonakinbiyi <akinbiyisolo22@gmail.com>
2022-07-23 20:25:19 +0100

    setting up env for postgresql db on-cloud: heroku ~~ 6 hopefully it works

--- db.js
+++ db.js
@@ -9,7 +9,7 @@ const devConfig = {
 };
 
 const proConfig = {
-  connectionString: process.env.DATABASE_URL,
+  connectionString: `${process.env.DATABASE_URL}?ssl=true`,
 };
 
 const pool = new Pool(
