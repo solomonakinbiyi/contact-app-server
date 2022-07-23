6a9bd5f0793d5d25deb9ec169e5c62a0b323118c
solomonakinbiyi <akinbiyisolo22@gmail.com>
2022-07-23 19:40:46 +0100

    setting up env for postgresql db on-cloud: heroku

--- db.js
+++ db.js
@@ -1,10 +1,19 @@
 const Pool = require("pg").Pool;
 
-const pool = new Pool({
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  port: process.env.DBPORT,
-  database: process.env.DATABASE_URL,
-});
+const devConfig = {
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  port: process.env.PG_PORT,
+};
+
+const proConfig = {
+  connectionString: process.env.DATABASE_URL,
+};
+
+const pool = new Pool(
+  process.env.NODE_ENV === "production" ? proConfig : devConfig
+);
 
 module.exports = pool;
